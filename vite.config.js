458bfdbc20548a8a54dc2ee8c8e876ff22db2540
alanyscesario249-create <alanyscesario249@gmail.com>
2026-04-18 13:26:54 -0300

    base do site

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/Proximo-Amor-2/',
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'sobre.html'),
+        login: resolve(__dirname, 'login.html'),
+        signup: resolve(__dirname, 'cadastro.html'),
+        jobs: resolve(__dirname, 'vagas.html'),
+        calendar: resolve(__dirname, 'calendario.html'),
+        map: resolve(__dirname, 'mapa.html'),
+      },
+    },
+  },
+});

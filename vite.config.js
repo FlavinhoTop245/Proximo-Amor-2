44216d1d3575bf02b76fe9c2e442d492c3c4322d
Carlos <carlos2511760@gmail.com>
2026-04-18 13:40:00 -0300

    Fix: restaurar config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
+  base: '/Proximo-Amor-2/',
   build: {
     rollupOptions: {
       input: {

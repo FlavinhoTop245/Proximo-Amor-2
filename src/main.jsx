a10fd32cd3254e76a33a1569af9ea02e173afc38
Carlos <carlos2511760@gmail.com>
2026-04-19 11:47:17 -0300

    Fix: Padronizando caminhos de CSS para deploy na Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '../assets/index-DvgdfjtO.css'
+import './index.css'
 import App from './App.jsx'
 import { LanguageProvider } from './contexts/LanguageContext.jsx'
 import { AuthProvider } from './contexts/AuthContext.jsx'
